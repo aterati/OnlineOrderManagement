c1b7ba5ed78236157cf7f090329b9d78fbe6eb6f
Arvind <arv.terati@gmail.com>
2016-09-25 03:18:18 -0700

    Improved css.
    Improved user experience:
    	- User can now select the order item and update the order quantity
    directly from the Order List.
    	
    	- Remove button and check box are replaced with a remove icon right
    beside each order. User can remove an order from the list with just a
    click.
    	
    	- No duplicates are allowed in the order list.
    	
    	- Added images for all the order items.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,78 @@
-var app= angular.module("myApp",[]);
+var app = angular.module("myApp", []);
+	app.controller("myCtrl", function($scope) {
+		$scope.orders = [];
+		$scope.cart = [];
+		$scope.enableSend = false;
+		$scope.isOrderSubmitted = false;
+		var count = 0;
 
-angular.controller("myController",function($scope){
-	$scope.add = function(){
-		$scope.order=$scope.order-item;
-	};
-	
-});
+		$scope.phonePattern = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
+
+		$scope.addItems = function() {
+
+			var orderItem = $scope.orderItem;
+			isOrderItemDup = false;
+			count++;
+			
+			if($scope.isOrderSubmitted)
+			{
+				$scope.orders = [];
+			}
+
+			angular.forEach($scope.orders, function(order) {
+				if (order.orderItem == orderItem)
+					isOrderItemDup = true;
+			})
+
+			if (!isOrderItemDup) {
+				$scope.orders.push({
+					id : count,
+					orderItem : orderItem,
+					orderQuantity : 1,
+					remove : false
+				});
+			$scope.enableSend = true;
+			}
+		}
+
+		$scope.updateQuantity = function() {
+
+			var orderQuantity = $scope.orderQuantity;
+			var orderItem = $scope.orderItem;
+
+			var oldOrders = $scope.orders;
+			$scope.orders = [];
+
+			angular.forEach(oldOrders, function(order) {
+				order.orderQuantity = orderQuantity;
+				$scope.orders.push(order);
+
+			});
+		}
+
+		$scope.removeItems = function(id) {
+
+			var oldOrders = $scope.orders;
+			$scope.orders = [];
+
+			angular.forEach(oldOrders, function(order) {
+				if (order.id!=id)
+					$scope.orders.push(order);
+
+			});
+
+			if ($scope.orders.length == 0)
+				$scope.enableSend = false;
+		}
+
+		$scope.submitOrder = function() {
+			$scope.success = true;
+			$scope.enableSend = false;
+			$scope.isOrderSubmitted = true;
+		}
+
+		$scope.cancelOrder = function() {
+			$scope.success = false;
+			$scope.orders = [];
+		}
+	});
